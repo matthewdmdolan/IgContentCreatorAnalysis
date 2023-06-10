ccf047c5d33c2cc2871d3e548cb54245d2cbb37f
Matt Dolan <60691539+matthewdmdolan@users.noreply.github.com>
2023-06-10 14:37:44 +0100

    Add files via upload

--- Dashboard.py
+++ Dashboard.py
@@ -1,133 +1,301 @@
-import plotly.graph_objects as go
-import plotly.express as px
+import re
+from collections import Counter
+
+import matplotlib.pyplot as plt
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import seaborn as sns
+from flair.data import Sentence
+from flair.models import TextClassifier
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from plotly.subplots import make_subplots
+from sklearn.linear_model import LinearRegression
 
-# Assuming you have a DataFrame called 'df' containing your data
+import plotly.graph_objects as go
+import plotly.io as pio
 
-df_nlp = 0
+import plotly.io as plt_io
+import plotly.express as px
 
-# Visualization 1: Bar Chart
-fig1 = px.bar(df, x='Category', y='Count', color='Category')
-fig1.update_layout(title='Category Counts')
+# create our custom_dark theme from the plotly_dark template
+plt_io.templates["custom_dark"] = plt_io.templates["plotly_dark"]
 
-# Visualization 2: Line Chart
-fig2 = go.Figure()
-fig2.add_trace(go.Scatter(x=df['Date'], y=df['Impressions'], mode='lines', name='Impressions'))
-fig2.add_trace(go.Scatter(x=df['Date'], y=df['Likes'], mode='lines', name='Likes'))
-fig2.update_layout(title='Impressions and Likes Over Time')
+plotly_template = pio.templates["plotly_dark"]
+print (plotly_template)
 
-# Visualization 3: Scatter Plot
-fig3 = px.scatter(df, x='Followers', y='Likes', color='Category')
-fig3.update_layout(title='Followers vs Likes')
 
-# Visualization 4: Pie Chart
-fig4 = px.pie(df, names='Category', values='Count')
-fig4.update_layout(title='Category Distribution')
 
-# Create the dashboard layout
-dashboard = go.Figure()
 
-# Add visualizations to the dashboard layout
-dashboard.add_trace(go.Scatter(
-    x=[0, 0.5, 0.5, 0],
-    y=[0.5, 0.5, 0, 0],
-    fill="toself",
-    fillcolor="rgba(0, 0, 0, 0)",
-    hoveron="fills",
-    line=dict(color='rgba(0, 0, 0, 0)'),
-    name='visualization_1',
-    showlegend=False,
-    mode="lines",
-))
+# Function to install missing packages
+# Assuming you have two visualizations: fig1 and fig2
+# Function to read CSV file
+def read_csv(filepath, encodings=['utf-8', 'latin1', 'iso-8859-1']):
+    try:
+        for encoding in encodings:
+            try:
+                df = pd.read_csv(filepath, encoding=encoding)
+                return df
+            except UnicodeDecodeError:
+                continue
+        raise Exception(f"Unable to decode the file using the specified encodings: {', '.join(encodings)}")
+    except FileNotFoundError:
+        print("File not found. Please check the filepath.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return None
 
-dashboard.add_trace(go.Scatter(
-    x=[0.5, 1, 1, 0.5],
-    y=[0.5, 0.5, 0, 0],
-    fill="toself",
-    fillcolor="rgba(0, 0, 0, 0)",
-    hoveron="fills",
-    line=dict(color='rgba(0, 0, 0, 0)'),
-    name='visualization_2',
-    showlegend=False,
-    mode="lines",
-))
+# Reading in file:
+filepath = '/Users/mattdolan/Documents/pythondata/Instagram data.csv'
+df = read_csv(filepath)
+print(df)
 
-dashboard.add_trace(go.Scatter(
-    x=[0, 0.5, 0.5, 0],
-    y=[1, 1, 0.5, 0.5],
-    fill="toself",
-    fillcolor="rgba(0, 0, 0, 0)",
-    hoveron="fills",
-    line=dict(color='rgba(0, 0, 0, 0)'),
-    name='visualization_3',
-    showlegend=False,
-    mode="lines",
-))
+#Create the subplots with 1 row and 3 columns
+fig = make_subplots(rows=2, cols=3)
 
-dashboard.add_trace(go.Scatter(
-    x=[0.5, 1, 1, 0.5],
-    y=[1, 1, 0.5, 0.5],
-    fill="toself",
-    fillcolor="rgba(0, 0, 0, 0)",
-    hoveron="fills",
-    line=dict(color='rgba(0, 0, 0, 0)'),
-    name='visualization_4',
-    showlegend=False,
-    mode="lines",
-))
+#dropping nans
+df = df.dropna()
 
-# Update the dashboard layout with visualizations
-dashboard.update_layout(
-    title='Dashboard',
-    xaxis=dict(visible=False),
-    yaxis=dict(visible=False),
-    showlegend=False,
+def lowercase_columns(df):
+    df.columns = df.columns.str.lower()
+    return df
+
+# calling function to make column names lower-case
+lowercase_columns(df)
+print(df)
+
+# function for data cleaning for qualitative columns
+def prepare_text_column(column):
+    # Lowercase the text
+    column = column.str.lower()
+    # Remove special characters and digits
+    column = column.apply(lambda x: re.sub(r'[^a-zA-Z\s]', '', x))
+
+    # Tokenize the text
+    column = column.apply(lambda x: word_tokenize(x))
+
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    column = column.apply(lambda x: [word for word in x if word not in stop_words])
+
+    # Lemmatize the words
+    lemmatizer = WordNetLemmatizer()
+    column = column.apply(lambda x: [lemmatizer.lemmatize(word) for word in x])
+
+    # Join the words back into a sentence
+    column = column.apply(lambda x: ' '.join(x))
+
+    return column
+
+# using function on qualitative columns to clean for best analysis
+df['hashtags'] = prepare_text_column(df['hashtags'])
+df['caption'] = prepare_text_column(df['caption'])
+
+def generate_word_counts(column, group_column=None):
+    # Combine all the text from the column into a single string
+    text = ' '.join(column)
+
+    # Split the text into individual words
+    words = text.split()
+
+    # Create a frequency distribution of the words
+    word_counts = pd.Series(words).value_counts()
+    return word_counts
+
+# Generate word counts for 'impressions' column, grouped by 'hashtags' column
+word_counts1 = generate_word_counts(df['hashtags'], group_column=df['impressions'])
+print(word_counts1)
+
+# Create a bar chart
+fig1 = px.bar(word_counts1, x=word_counts1.index, y=word_counts1.values)
+
+# Update the layout of the chart
+fig1.update_layout(
+    title='Word Counts',
+    xaxis=dict(title='Words'),
+    yaxis=dict(title='Count')
 )
 
-# Add subplots to the dashboard layout
-dashboard.update_layout(
-    annotations=[
-        go.layout.Annotation(
-            x=0.25,
-            y=0.75,
-            xref="paper",
-            yref="paper",
-            text="Visualization 1",
-            showarrow=False,
-        ),
-        go.layout.Annotation(
-            x=0.75,
-            y=0.75,
-            xref="paper",
-            yref="paper",
-            text="Visualization 2",
-            showarrow=False,
-        ),
-        go.layout.Annotation(
-            x=0.25,
-            y=0.25,
-            xref="paper",
-            yref="paper",
-            text="Visualization 3",
-            showarrow=False,
-        ),
-        go.layout.Annotation(
-            x=0.75,
-            y=0.25,
-            xref="paper",
-            yref="paper",
-            text="Visualization 4",
-            showarrow=False,
-        ),
-    ]
+# Generate word counts for 'impressions' column, grouped by 'hashtags' column
+word_counts2 = generate_word_counts(df['caption'], group_column=df['impressions'])
+print(word_counts2)
+
+# Create a bar chart
+fig2 = px.bar(word_counts2, x=word_counts2.index, y=word_counts2.values)
+print(fig2)
+
+# Update the layout of the chart
+fig2.update_layout(
+    title='Word Counts',
+    xaxis=dict(title='Words'),
+    yaxis=dict(title='Count')
 )
 
-# Display the dashboard
-dashboard.show()
+# looking at location of engagements:
+home = df["from home"].sum()
+hashtags = df["from hashtags"].sum()
+explore = df["from explore"].sum()
+other = df["from other"].sum()
+
+labels = ['From Home', 'From Hashtags', 'From Explore', 'Other']
+values = [home, hashtags, explore, other]
+
+##new
+df_nlp = df.copy()
+
+print(df_nlp)
+
+# defining columns we want to keep
+cols_nlp = ['impressions', 'likes', 'follows', 'caption', 'hashtags']
+
+# Filter the DataFrame to keep only the specified columns
+df_nlp = df_nlp.reindex(columns=cols_nlp, fill_value=None)
+
+print(df_nlp)
+
+# wordcloud generation function
+def generate_wordcloud(data_column):
+    # Flatten the list of tokens into a single list
+    flattened_tokens = [token for sublist in data_column for token in sublist]
+
+    # Count the frequency of each word
+    word_counts = Counter(flattened_tokens)
+
+    # Generate the word cloud
+    wordcloud = ' '.join(flattened_tokens)
+
+    # Create a bar chart of the word frequencies
+    plt.figure(figsize=(10, 6))
+    plt.bar(word_counts.keys(), word_counts.values())
+    plt.xlabel('Words')
+    plt.ylabel('Frequency')
+    plt.title('Word Frequency')
+    plt.xticks(rotation=90)
+    #plt.show()
+
+# Sentiment Analysis - Caption
+# Load the sentiment classification model
+sia = TextClassifier.load('en-sentiment')
+
+def flair_prediction(column):
+    sentiment_scores = []
+
+    for text in column:
+        sentence = Sentence(text)
+        sia.predict(sentence)
+        score = sentence.labels[0].score
+        sentiment_scores.append(score)
+
+    return sentiment_scores
+
+# Caption analysis
+# Apply flair_prediction function to the caption field and store the sentiment scores in a new column
+# Extract the independent variable (X) and dependent variable (y)
+df_nlp['sentiment_score_caption'] = flair_prediction(df_nlp['caption'])
+y = df_nlp['impressions'].values
+X = df_nlp['sentiment_score_caption'].values.reshape(-1, 1)
+model = LinearRegression()
+model.fit(X, y)
+y_pred = model.predict(X)
+
+# Create subplots with 2 rows and 1 column
+fig = make_subplots(rows=2, cols=3)
+
+#Hashtag Regression Analysis
+df_nlp['sentiment_score_hashtags'] = flair_prediction(df_nlp['hashtags'])
+y = df_nlp['impressions'].values
+X = df_nlp['sentiment_score_hashtags'].values.reshape(-1, 1)
+model = LinearRegression()
+model.fit(X, y)
+y_pred = model.predict(X)
+# plot regression
+fig5 = plt.scatter(X, y, color='blue', label='Actual')
+#fig5 = plt.plot(X, y_pred, color='red', label='Regression Line')
+plt.xlabel('Sentiment Score')
+plt.ylabel('Impressions')
+plt.title('Simple Linear Regression')
+plt.legend()
+#plt.show()
+
+# Add traces to the subplot
+#fig.add_trace(scatter_trace, row=2, col=1)
+#fig.add_trace(regression_trace, row=2, col=1)
+
+#formatting dashboard
+
+#Create the subplots with 2 rows and 3 columns
+fig = make_subplots(rows=2, cols=3)
+
+# Add fig1 to the first subplot
+for trace in fig1.data:
+    fig.add_trace(trace, row=1, col=1)
+
+# Add fig2 to the second subplot
+for trace in fig2.data:
+    fig.add_trace(trace, row=1, col=2)
+
+#creating fig.3
+fig3 = go.Scatter(x=X, y=y, mode='markers', marker=dict(color='blue'), name='Actual')
+#regression_trace = go.Scatter(x=X, y=y_pred, mode='lines', line=dict(color='red'), name='Regression Line')
+fig.add_trace(fig3, row=2, col=1)
+
+# Data for the bar plot
+labels = ['From Home', 'From Hashtags', 'From Explore', 'Other']
+values = [home, hashtags, explore, other]
+
+# Create the bar plot trace
+bar_trace = go.Bar(x=values, y=labels, orientation='h')
+
+# Add the bar plot trace to the figure
+fig.add_trace(bar_trace, row=2, col=2)
+
+# Update the layout
+# Create menu bar
+menu_bar = go.FigureWidget([go.Bar(x=['Home', 'Hashtags', 'Explore', 'Other'], y=[home, hashtags, explore, other],
+                                  marker=dict(color=['#002633', '#005A80', '#0088B0', '#00B4CC']),
+                                  textposition='auto', hoverinfo='none')])
+
+# Update menu bar layout
+menu_bar.update_layout(
+    title='Menu',
+    xaxis=dict(title='Menu Options'),
+    yaxis=dict(title='Counts'),
+    plot_bgcolor='#F8F8F8',
+    paper_bgcolor='#F8F8F8',
+    height=300,
+    margin=dict(l=20, r=20, t=60, b=20),
+)
+
+# Create menu bar
+menu_bar = go.FigureWidget([go.Bar(x=['Home', 'Hashtags', 'Explore', 'Other'], y=[home, hashtags, explore, other],
+                                  marker=dict(color=['#002633', '#005A80', '#0088B0', '#00B4CC']),
+                                  textposition='auto', hoverinfo='none')])
+
+# Update menu bar layout
+menu_bar.update_layout(
+    title='Menu',
+    xaxis=dict(title='Menu Options'),
+    yaxis=dict(title='Counts'),
+    #plot_bgcolor='#F8F8F8',
+    #paper_bgcolor='#F8F8F8',
+    height=300,
+    margin=dict(l=20, r=20, t=0, b=0),
+)
+
+# Update the layout of the dashboard
+
+# Update the layout
+fig.update_layout(title="Dashboard",
+    showlegend=False,
+    grid=dict(rows=2, columns=3),
+    width=900,
+    height=800,
+template = 'plotly_dark'
+)
 
 
-
-
-
-
+# Show the dashboard
+fig.show()
 
--- Eda.py
+++ Eda.py
@@ -1,6 +1,5 @@
-
-#Instagram Data Analysis#
-#Author: Matthew Dolan
+# Instagram Data Analysis#
+# Author: Matthew Dolan
 
 # Features:
 # - Impressions: Number of impressions in a post (Reach)
@@ -18,17 +17,26 @@
 # - Hashtags: Hashtags used in the post
 
 import importlib
+import re
 import subprocess
 import sys
+import numpy as np
+from collections import Counter
+import matplotlib.pyplot as plt
 import pandas as pd
+import plotly.express as px
 import seaborn as sns
-import nltk
 from flair.data import Sentence
 from flair.models import TextClassifier
-from sklearn.linear_model import LinearRegression
-import matplotlib.pyplot as plt
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sklearn.linear_model import LinearRegression, PassiveAggressiveRegressor
 
 # Function to install missing packages
+from sklearn.model_selection import train_test_split
+
+
 def install_packages(packages):
     for package in packages:
         try:
@@ -39,9 +47,11 @@ def install_packages(packages):
             subprocess.check_call([sys.executable, "-m", "pip", "install", package])
             print(f"{package} has been successfully installed and imported.")
 
+
 # List of packages necessary to be installed
 package_list = ["numpy", "seaborn", "matplotlib", "matplotlib.pyplot", "ipython", "nltk"]
 install_packages(package_list)
+
 
 # Function to read CSV file
 def read_csv(filepath, encodings=['utf-8', 'latin1', 'iso-8859-1']):
@@ -59,7 +69,8 @@ def read_csv(filepath, encodings=['utf-8', 'latin1', 'iso-8859-1']):
         print(f"An error occurred: {str(e)}")
         return None
 
-# Example usage:
+
+# Reading in file:
 filepath = '/Users/mattdolan/Documents/pythondata/Instagram data.csv'
 df = read_csv(filepath)
 
@@ -67,15 +78,15 @@ df = read_csv(filepath)
 if df is not None:
     print(df.head(5))
 
-#checking column names
+# checking column names
 col_names = print(list(df.columns.values))
 
-#displaying all data for eda for clearer view of df
+# displaying all data for eda for clearer view of df
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-    print(df)
+    print(df.head(5))
 
-#function that provides a list of null and non-null counts per column and provides list of distinct values
-#less insightful for caption as variance in responses
+# function that provides a list of null and non-null counts per column and provides list of distinct values
+# less insightful for caption as variance in responses
 def analyze_dataframe(df):
     # Count null and non-null values for each column
     null_counts = df.isnull().sum()
@@ -93,38 +104,42 @@ def analyze_dataframe(df):
         distinct_values = df[column].unique()
         print(f"{column}: {distinct_values}")
 
-
 # Call the function to analyze the DataFrame
 analyze_dataframe(df)
+
+
+#looking at location of engagements:
+home = df["From Home"].sum()
+hashtags = df["From Hashtags"].sum()
+explore = df["From Explore"].sum()
+other = df["From Other"].sum()
+
+labels = ['From Home','From Hashtags','From Explore','Other']
+labels = ['From Home','From Hashtags','From Explore','Other']
+values = [home, hashtags, explore, other]
+
+fig = px.pie(df, values=values, names=labels,
+             title='Impressions on Instagram Posts From Various Sources', hole=0.5)
+fig.show()
+
+
 
 def lowercase_columns(df):
     df.columns = df.columns.str.lower()
     return df
-
+#calling function to make column names lower-case
 lowercase_columns(df)
-
 print(df)
 
-#MD - how hashtags and captions play a role in user engagement?
-#MD-  From an impressions (interactive perspective), profile visits and also number of followers
-
-
 # List of columns to keep
-#impressions and profile visits as measurement variables?
-columns_to_keep = ['impressions', 'saves', 'comments', 'shares', 'likes', 'profile visits', 'follows', 'caption', 'hashtags']
-
+columns_to_keep = ['impressions', 'saves', 'comments', 'shares', 'likes', 'profile visits', 'follows', 'caption',
+                   'hashtags']
 # Filter the DataFrame to keep only the specified columns
 df2 = df.reindex(columns=columns_to_keep, fill_value=None)
-
 # Print the resulting DataFrame
 print(df2)
 
-#function for data cleaning for nlp
-import re
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-
+# function for data cleaning for qualitative columns
 def prepare_text_column(column):
     # Lowercase the text
     column = column.str.lower()
@@ -146,57 +161,101 @@ def prepare_text_column(column):
     column = column.apply(lambda x: ' '.join(x))
 
     return column
-
-#using function on columns that may be of use for our analysis
+# using function on qualitative columns to clean for best analysis
 df2['hashtags'] = prepare_text_column(df2['hashtags'])
 df2['caption'] = prepare_text_column(df2['caption'])
+df2.head()
 
-print(df2)
+##EDA
 
-##EDAAA
+#generating bar chart coints for popular words used in each qualitative field
+def generate_word_counts(column):
+    # Combine all the text from the column into a single string
+    text = ' '.join(column)
 
-#defining list of columns for value count
-#profile_visits = associated with impresssions = removed -> could be different hypothesis if later used
-#relationship between sentiment scores and profile visits e.g.
+    # Split the text into individual words
+    words = text.split()
+
+    # Create a frequency distribution of the words
+    word_counts = pd.Series(words).value_counts()
+
+    # Plot the bar chart
+    plt.figure(figsize=(10, 6))
+    word_counts[:20].plot(kind='bar')
+    plt.xlabel('Words')
+    plt.ylabel('Count')
+    plt.title('Top 20 Word Counts')
+
+generate_word_counts(df2['hashtags'])
+generate_word_counts(df2['caption'])
+
+# defining list of columns for value count
+# profile_visits = associated with impresssions = removed -> could be different hypothesis if later used
+# relationship between sentiment scores and profile visits e.g.
 def plot_counts(df, col):
-   plt.figure(figsize=(9, 9))
-   sns.countplot(data=df, y=col,
-                 order=df[col].value_counts().index[:50])
-   Title = f'{col}'
-   plt.title(Title, fontsize=15)
-   plt.show()
+    plt.figure(figsize=(9, 9))
+    sns.countplot(data=df, y=col,
+                  order=df[col].value_counts().index[:50])
+    Title = f'{col}'
+    plt.title(Title, fontsize=15)
+    plt.show()
 
-#defining list of columns for value count
-#profile_visits = associated with impresssions = removed -> could be different hypothesis if later used
-#follows included as direct involvement
-#relationship between sentiment scores and profile visits e.g.
-#taken out 'saves', 'comments', 'shares', 'likes' - impressions gives us an overview already
-#can drilldown for further analysis
+# defining list of columns for value count
+# profile_visits = associated with impresssions = removed -> could be different hypothesis if later used
+# follows included as direct involvement
+# relationship between sentiment scores and profile visits e.g.
+# taken out 'saves', 'comments', 'shares', 'likes' - impressions gives us an overview already
+# can drilldown for further analysis
+# MD - how hashtags and captions play a role in user engagement?
+# MD-  From an impressions (interactive perspective), profile visits and also number of followers
 cols = ['impressions', 'profile visits', 'follows', 'caption', 'hashtags']
 
-#producing count of different values in above defined columns
+# producing count of different values in above defined columns
 for col in cols:
     plot_counts(df2, col)
 
 print(df2)
+
+
 
 ##Nlp Analysis
 df_nlp = df2.copy()
 
 print(df_nlp)
 
-#defining columns we want to keep
+# defining columns we want to keep
 cols_nlp = ['impressions', 'likes', 'follows', 'caption', 'hashtags']
 
-#Filter the DataFrame to keep only the specified columns
+# Filter the DataFrame to keep only the specified columns
 df_nlp = df_nlp.reindex(columns=cols_nlp, fill_value=None)
 
 print(df_nlp)
 
-##remove when not using to scale due to limiting sample size
+# wordcloud generation function
+def generate_wordcloud(data_column):
+    # Flatten the list of tokens into a single list
+    flattened_tokens = [token for sublist in data_column for token in sublist]
 
-#Sentiment Analysis - Caption
+    # Count the frequency of each word
+    word_counts = Counter(flattened_tokens)
 
+    # Generate the word cloud
+    wordcloud = ' '.join(flattened_tokens)
+
+    # Create a bar chart of the word frequencies
+    plt.figure(figsize=(10, 6))
+    plt.bar(word_counts.keys(), word_counts.values())
+    plt.xlabel('Words')
+    plt.ylabel('Frequency')
+    plt.title('Word Frequency')
+    plt.xticks(rotation=90)
+    plt.show()
+
+# generating wordcloud for two columns of interest
+generate_wordcloud(df_nlp['caption'])
+generate_wordcloud(df_nlp['hashtags'])
+
+# Sentiment Analysis - Caption
 # Load the sentiment classification model
 sia = TextClassifier.load('en-sentiment')
 
@@ -211,37 +270,16 @@ def flair_prediction(column):
 
     return sentiment_scores
 
-#Caption**
+# Caption analysis
 # Apply flair_prediction function to the caption field and store the sentiment scores in a new column
-df_nlp['sentiment_score_caption'] = flair_prediction(df_nlp['caption'])
-
-#checking scores have saved and qcing transformations
-print(df_nlp)
-
-###positive relationship between sentiment score and no. of impressions
-#sns.scatterplot(data=df_nlp, x="sentiment_score", y="impressions")
-#plt.xlim(-1, 1)
-# Display the plot
-#plt.show()
-
 # Extract the independent variable (X) and dependent variable (y)
-###regression analysis sentimental and impressions
-
+df_nlp['sentiment_score_caption'] = flair_prediction(df_nlp['caption'])
 y = df_nlp['impressions'].values
 X = df_nlp['sentiment_score_caption'].values.reshape(-1, 1)
 model = LinearRegression()
-# Create an instance of the LinearRegression model
-
 model.fit(X, y)
-# Fit the model to the data
-
 y_pred = model.predict(X)
-# Predict the values using the trained model
-# Print the coefficients and intercept of the linear regression model
-print("Intercept:", model.intercept_)
-print("Coefficient:", model.coef_[0])
-
-#plot regression
+# plot regression
 plt.scatter(X, y, color='blue', label='Actual')
 plt.plot(X, y_pred, color='red', label='Regression Line')
 plt.xlabel('Sentiment Score')
@@ -250,40 +288,22 @@ plt.title('Simple Linear Regression')
 plt.legend()
 plt.show()
 
-
-
-#correlation matrix of results for quick touchpoint
-df_nlp_2 = pd.DataFrame(df_nlp)
-corr_matrix = df_nlp_2.corr()
+# correlation matrix of results for caption analysis touchpoint
+df_nlp_2_caption = pd.DataFrame(df_nlp)
+corr_matrix = df_nlp_2_caption.corr()
 print(corr_matrix)
 sns.heatmap(corr_matrix, annot=True)
 plt.show()
 
-
-df_nlp_2.to_csv('/Users/mattdolan/Documents/pythondata/Instagram data_qc.csv', index=False)
-
-
-##hashtag Regression Analysis
+# hashtag Regression Analysis
+# Extract the independent variable (X) and dependent variable (y)
 df_nlp['sentiment_score_hashtags'] = flair_prediction(df_nlp['hashtags'])
-
- # Extract the independent variable (X) and dependent variable (y)
- ###regression analysis sentimental and impressions
-
 y = df_nlp['impressions'].values
 X = df_nlp['sentiment_score_hashtags'].values.reshape(-1, 1)
-
 model = LinearRegression()
- # Create an instance of the LinearRegression model
-
 model.fit(X, y)
-
 y_pred = model.predict(X)
-# Predict the values using the trained model
-# Print the coefficients and intercept of the linear regression model
-print("Intercept:", model.intercept_)
-print("Coefficient:", model.coef_[0])
-
-#plot regression
+# plot regression
 plt.scatter(X, y, color='blue', label='Actual')
 plt.plot(X, y_pred, color='red', label='Regression Line')
 plt.xlabel('Sentiment Score')
@@ -291,3 +311,37 @@ plt.ylabel('Impressions')
 plt.title('Simple Linear Regression')
 plt.legend()
 plt.show()
+
+
+# correlation matrix of results for hashtag analysis touchpoint
+df_nlp_2_hashtag = pd.DataFrame(df_nlp)
+corr_matrix2 = df_nlp_2_hashtag .corr()
+print(corr_matrix2)
+sns.heatmap(corr_matrix2, annot=True)
+plt.show()
+
+#NLP Analysis v2
+
+
+
+
+##Analysis - Machine Learning
+##seeing how much engagement future posts would achieve based on various predictors
+
+x = np.array(df[['likes', 'saves', 'comments', 'shares',
+                   'profile visits', 'follows']])
+y = np.array(df["impressions"])
+xtrain, xtest, ytrain, ytest = train_test_split(x, y,
+                                                test_size=0.2,
+                                                random_state=42)
+
+model = PassiveAggressiveRegressor()
+model.fit(xtrain, ytrain)
+score = model.score(xtest, ytest)
+print(score)
+
+# Features = [['Likes','Saves', 'Comments', 'Shares', 'Profile Visits', 'Follows']]
+features = np.array([[282.0, 233.0, 4.0, 9.0, 165.0, 54.0]])
+predictions = model.predict(features)
+print(features)
+print(predictions)
